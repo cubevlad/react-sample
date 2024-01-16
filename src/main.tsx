957bc7d21243cafc3ee8acd47227c5799222c456
v.kubasov <v.kubasov@gaskar.group>
2024-01-16 10:16:52 +0400

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,8 @@ import ReactDOM from 'react-dom/client'
 import { App } from './app'
 import './styles/resetBrowserStyles.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+const root = document.getElementById('root')
+
+if (root) {
+  ReactDOM.createRoot(root).render(<App />)
+}
