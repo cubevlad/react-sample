093b31d5fd26dc540ac5e1e3c0737d4998fa8fd1
cubevlad <kubasovvv07@gmail.com>
2024-02-08 19:08:10 +0400

    chore: updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,110 @@ module.exports = {
   rules: {
     'import/dynamic-import-chunkname': 'off',
   },
+  // if you want apply rules for FSD just uncomment code below
+
+  // overrides: [
+  //   {
+  //     files: ['*.ts', '*.tsx'],
+  //     rules: {
+  //       'simple-import-sort/imports': [
+  //         'error',
+  //         {
+  //           groups: [
+  //             ['^react', '^@?\\w'],
+
+  //             ['^(client/domain)(/.*|$)'],
+  //             ['^(client/shared)(/.*|$)'],
+
+  //             ['^(client/app)(/.*|$)'],
+
+  //             ['^(client/processes)(/.*|$)'],
+  //             ['^(client/widgets)(/.*|$)'],
+  //             ['^(client/pages)(/.*|$)'],
+
+  //             ['^(client/entities)(/.*|$)'],
+  //             ['^(client/features)(/.*|$)'],
+
+  //             ['^(#server/routes)(/.*|$)'],
+
+  //             // Side effect imports.
+  //             ['^\\u0000'],
+  //             ['^.+\\.?(css)$'],
+  //           ],
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     files: './src/**/**.+(ts|tsx)',
+  //     rules: {
+  //       'feature-slice-isolation': [
+  //         'error',
+  //         {
+  //           layers: {
+  //             1: 'app',
+  //             2: 'processes',
+  //             3: 'pages',
+  //             4: 'widgets',
+  //             5: 'features',
+  //             6: 'entities',
+  //             7: 'shared',
+  //           },
+  //         },
+  //       ],
+  //       'isolation-import-paths': [
+  //         'error',
+  //         {
+  //           isAllowedInnerSlice: true,
+  //           layers: {
+  //             app: 1,
+  //             processes: 1,
+  //             pages: 1,
+  //             widgets: 1,
+  //             features: 1,
+  //             entities: 1,
+  //             domain: 1,
+  //           },
+  //         },
+  //       ],
+  //       'folder-file-required': [
+  //         'error',
+  //         {
+  //           validateFilenames: ['index'],
+  //           ignoreFolders: [
+  //             { folder: 'node_modules', deep: true },
+  //             { folder: '__snapshots__' },
+  //             { folder: 'icons' },
+  //             { folder: 'img' },
+  //           ],
+  //           folders: [
+  //             'app',
+  //             'processes',
+  //             'pages',
+  //             'widgets',
+  //             'features',
+  //             'entities',
+  //             'shared',
+  //             'domain',
+  //           ],
+  //           root: '/src',
+  //         },
+  //       ],
+  //       'react/display-name': 0,
+  //       'alias-import-paths': [
+  //         'error',
+  //         {
+  //           paths: [
+  //             {
+  //               ignoredFolders: [],
+  //               allowSameFolder: true,
+  //               rootDir: 'src',
+  //               prefix: 'client',
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     },
+  //   },
+  // ],
 }
